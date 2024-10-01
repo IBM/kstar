8b9749d75dc28bc7c5a0dffbf93896b335886972
Michael Katz <michael.katz1@ibm.com>
2024-10-01 21:27:13 +0000

    axioms experiment

---
+++ experiments/axioms/parser.py
@@ -0,0 +1,154 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import re
+import os
+import json
+import itertools
+
+from lab.parser import Parser
+
+_PLAN_INFO_REGEX = re.compile(r"; cost = (\d+) \((unit cost|general cost)\)\n")
+
+def get_plan_cost(path):
+    try:
+        with open(path) as input_file:
+            line = None
+            for line in input_file:
+                if line.strip().startswith(";"):
+                    continue
+            # line is now the last line
+            match = _PLAN_INFO_REGEX.match(line)
+            if match:
+                return int(match.group(1))
+            return None
+    except:
+        return None
+
+
+def get_plan_costs(plans_folder):
+    ret = []
+    for counter in itertools.count(1):
+        name = "sas_plan.%d" % counter
+        plan_filename = os.path.join(plans_folder, name)
+        if not os.path.exists(plan_filename):
+            break
+        cost = get_plan_cost(plan_filename)
+        if cost is not None:
+            ret.append(cost)
+    return ret
+
+
+def plans(content, props):
+    costs = get_plan_costs('found_plans')
+    props["plan_costs"] = costs
+    props["num_plans"] = len(costs)
+
+
+def get_data_from_static():
+    with open("static-properties", 'r') as sp:
+        return json.load(sp)
+
+
+def coverage(content, props):
+    props["coverage"] = int("total_time" in props)
+
+
+def kstar_coverage(content, props):
+    k_bound_reached = proved_no_more_plans= False
+    try:
+        if props["normal_termination"] == 1:
+            if props["num_plans"] >= props.get("k", 10000):
+                k_bound_reached = True
+            else:       
+                # kstar terminated normally before finding desired number of plans
+                proved_no_more_plans = True
+    except:
+        print("normal_termination missing -> can't compute kstar coverage")
+    else:    
+        props["kstar_coverage"] = int(k_bound_reached or proved_no_more_plans)
+        props["kstar_coverage_ratio"] = float("{:.3f}".format(float(props["num_plans"]) / float(props["k"])))
+
+
+def timer_values(content, props):
+    try:
+        props["after_goal_astar_time"] = props["total_astar_time"] - props["first_astar_time"]
+    except:
+        print("total/first_astar_time missing -> can't compute after_goal_astar_time")
+    
+    try:
+        props["total_kstar_time"] = props["total_astar_time"] + props["total_eppstein_time"]
+    except:
+        print("total_astar/eppstein_time missing -> can't compute total_kstar_time")
+
+def _get_states_pattern(attribute, name):
+    return (attribute, rf"{name}=(\d+) state\(s\)\.", int)
+
+first_astar_patterns = [
+    ("fist_astar_evaluations", r"first_astar::Evaluations:=(.+)\n", int),
+    _get_states_pattern("first_astar_dead_ends", "first_astar::Dead ends:"),
+    _get_states_pattern("first_astar_evaluated", "first_astar::Evaluated"),
+    _get_states_pattern("first_astar_expansions", "first_astar::Expanded"),
+    _get_states_pattern("first_astar_generated", "first_astar::Generated"),
+    _get_states_pattern("first_astar_reopened", "first_astar::Reopened"),
+    _get_states_pattern("first_astar_evaluations_until_last_jump", "Evaluated until last jump:"),
+    _get_states_pattern("first_astar_expansions_until_last_jump", "Expanded until last jump:"),
+    _get_states_pattern("first_astar_generated_until_last_jump", "Generated until last jump:"),
+    _get_states_pattern("first_astar_reopened_until_last_jump", "Reopened until last jump:")
+]
+
+
+def record_runfolder(content, props):
+    props["run_folder"] = os.getcwd()
+
+
+def multiplan_validation(content, props):
+    # from validate_multiplan_solutions.validate_run import main as validate_run_main
+    # class TempArg:
+    #     def __init__(self):
+    #         pass
+    # args = TempArg()
+    # args.planner="topk"
+    # args.number_of_plans = props["k"]
+    # args.run_folder = props["run_folder"]
+    # args.plans_folder = "found_plans"
+    # ret_code = validate_run_main(args)    
+    # while ret_code == "RERUN":
+    #     ret_code = validate_run_main(args)
+    # if ret_code == "PASSED":
+    #     props["validation_result"] = 1
+    # else:
+    #     props["validation_result"] = 0
+    props["validation_result"] = 0
+    
+
+class KstarParser(Parser):
+    def __init__(self):
+        Parser.__init__(self)
+
+        self.add_pattern("k", r"initialize::top-(\d+)", type=int)
+        self.add_pattern("normal_termination", r"normal_termination=(\d+)", type=int)
+        # steps
+        self.add_pattern("total_steps", r"search::total_step_iter=(\d+)", type=int)
+        self.add_pattern("total_astar_steps", r"search::total_num_astar_calls=(\d+)", type=int)
+        self.add_pattern("total_eppsetein_steps", r"search::total_num_eppstein_calls=(\d+)", type=int)
+        # times
+        self.add_pattern("first_astar_time", r"first_astar::stop=(.+)s", type=float)
+        self.add_pattern("total_astar_time", r"search::total astar time=(.+)s", type=float)
+        self.add_pattern("total_eppstein_time", r"search::total eppstein time=(.+)s", type=float)
+        self.add_pattern('search_time', r'Search time: (.+)s', required=False, type=float)
+        self.add_pattern('total_time', r'Total time: (.+)s', required=False, type=float)
+        self.add_pattern('first_plan_total_time', r'Total first plan time: (.+)s', required=False, type=float)
+
+        for name, pattern, typ in first_astar_patterns:
+            self.add_pattern(name, pattern, type=typ)
+
+        self.add_function(plans)
+        self.add_function(kstar_coverage)
+        self.add_function(timer_values)
+        self.add_function(record_runfolder)
+        self.add_function(coverage)
+
+        self.add_function(multiplan_validation)
+
---
+++ experiments/axioms/topk-sym-blind-3.5G-v2.py
@@ -0,0 +1,115 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import itertools
+import os
+
+from lab.environments import LocalEnvironment
+# from lab.reports import Attribute, geometric_mean
+# from downward.reports.compare import ComparativeReport
+from parser import KstarParser
+import common_setup
+from common_setup import IssueConfig, IssueExperiment
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+BENCHMARKS_DIR = '/data/software/fd-symbolic-axioms/benchmarks'
+REVISIONS = ["axioms"]
+REVISIONS = ["88043694f02c44871b5fd31fdfd1fcdf4538a8f6"]
+BUILDS = ["release"]
+
+exp_k = 1000
+CONFIG_NICKS = []
+
+CONFIG_NICKS.append(('kstar', ['--search', f'kstar(blind(), k={exp_k})']))
+CONFIG_NICKS.append(('okstar', ["--symmetries", "sym=structural_symmetries(time_bound=0,search_symmetries=oss,stabilize_initial_state=false,keep_operator_symmetries=true)",
+                '--search', f'kstar(blind(), symmetries=sym, k={exp_k})']))
+
+CONFIGS = [
+    IssueConfig(
+        config_nick,
+        config,
+        build_options=[build],
+        driver_options=["--build", build])
+    for build in BUILDS
+    for config_nick, config in CONFIG_NICKS
+]
+
+SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
+
+# SUITE = ['grid', 'gripper', 'blocks']
+
+SUITE = ['blocks-axioms', 'grid-axioms', 'miconic-axioms', 'optical-telegraphs', 'psr-middle', 'psr-large', 'philosophers', 'assembly', 'airport-adl', 'trucks',  'blocker', 'social-planning', 'sokoban-axioms', 'acc-cc2', 'grid-cc2', 'collab-and-comm', 'muddy-children', 'muddy-child', 'sum', 'word-rooms']
+
+
+ENVIRONMENT = LocalEnvironment(processes=48)
+
+exp = IssueExperiment(
+    revisions=REVISIONS,
+    configs=CONFIGS,
+    environment=ENVIRONMENT,
+    time_limit="30m",       # this soft-kills running executable
+    memory_limit="3584M"
+)
+exp.set_property("k", exp_k)
+exp.set_property("planner_time_limit", 1800)     # pass this to executable
+exp.set_property("planner_memory_limit", "3.5g")
+
+exp.add_suite(BENCHMARKS_DIR, SUITE)
+
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
+exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+exp.add_parser(exp.PLANNER_PARSER)
+exp.add_parser(KstarParser())
+
+exp.add_step('build', exp.build)
+exp.add_step('start', exp.start_runs)
+exp.add_step("parse", exp.parse)
+
+exp.add_fetcher(name='fetch')
+# exp.add_parse_again_step()
+
+KSTAR_ATTRIBUTES = [
+    # plans
+    "num_plans",
+    "num_found_plans",
+    "dump_plans",
+    "stop_on_k",
+    "stop_on_q",
+    "kstar_coverage",
+    "kstar_coverage_ratio",
+    # time in sec
+    "total_kstar_time",
+    "total_astar_time",
+    "total_eppsetein_time",
+    "first_astar_time",
+    "after_goal_astar_time",
+    # steps or iterations
+    "total_steps",
+    "total_astar_steps",
+    "total_eppsetein_steps",
+    #
+    "first_astar_dead_ends",
+    "first_astar_evaluated",
+    "first_astar_expansions",
+    "first_astar_generated",
+    "first_astar_reopened",
+    "first_astar_evaluations_until_last_jump",
+    "first_astar_expansions_until_last_jump",
+    "first_astar_generated_until_last_jump",
+    "first_astar_reopened_until_last_jump",
+    #
+    "validation_result",
+    "first_plan_total_time"
+]
+
+IssueExperiment.DEFAULT_TABLE_ATTRIBUTES += KSTAR_ATTRIBUTES
+attributes = IssueExperiment.DEFAULT_TABLE_ATTRIBUTES
+# exp.add_comparison_table_step(attributes=attributes)
+exp.add_absolute_report_step(attributes=attributes)
+
+exp.run_steps()
+
+
